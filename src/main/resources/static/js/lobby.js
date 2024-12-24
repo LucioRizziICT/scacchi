1966766db8904220cc7b9a6caa02c11e66a73d79
Lucio Rizzi <lucio.rizzi@ict-service.it>
2024-12-24 08:52:50 +0100

    feat: add invite modal and fix responsive topnav

--- src/main/resources/static/js/lobby.js
+++ src/main/resources/static/js/lobby.js
@@ -1,11 +1,48 @@
 const lobbyInfo = document.getElementById("lobbyInfo");
 
-lobbyInfo.addEventListener("resize", function() { //TODO: non listena una ceppa, da mettere ResizeObserver
-    if(lobbyInfo.clientWidth < 500) {
-        document.getElementById("topnav-icon").style.display = "block";
-        document.getElementById("topnav-label").style.display = "none";
-    } else {
-        document.getElementById("topnav-icon").style.display = "none";
-        document.getElementById("topnav-label").style.display = "block";
+const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+        if (entry.contentRect.width < 500) {
+            document.getElementById("topnav-icon").style.display = "block";
+            document.getElementById("topnav-label").style.display = "none";
+        } else {
+            document.getElementById("topnav-icon").style.display = "none";
+            document.getElementById("topnav-label").style.display = "block";
+        }
     }
 });
+
+resizeObserver.observe(lobbyInfo);
+
+const inviteModal = document.getElementById("inviteModal");
+
+function openInviteModal() {
+    inviteModal.style.display = "block";
+}
+
+function closeInviteModal() {
+    inviteModal.style.display = "none";
+}
+
+function writeInviteModalLink() {
+    const inviteLink = document.getElementById("inviteModalLink");
+    inviteLink.value = window.location.href;
+    console.log(inviteLink.value);
+}
+
+window.onclick = function(event) {
+    checkCloseModal(event);
+}
+
+function checkCloseModal(event) {
+    if(event.target === inviteModal) {
+        closeInviteModal();
+    }
+}
+
+function copyInviteLink() {
+    const inviteLink = document.getElementById("inviteModalLink");
+    inviteLink.select();
+    inviteLink.setSelectionRange(0, 99999);
+    document.execCommand("copy");
+}
